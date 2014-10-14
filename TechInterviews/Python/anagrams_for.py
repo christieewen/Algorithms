deea4ffe6e7cccebd26af2a66a09dac82ce01147
Christie Ewen <christieewen@users.noreply.github.com>
2014-10-14 14:50:12 -0400

    Create anagrams_for.py
    Framework to get started.

---
+++ TechInterviews/Python/anagrams_for.py
@@ -0,0 +1,12 @@
+from nltk.corpus import words
+from itertools import permutations
+
+def anagrams_for(word):
+  # TODO: 
+  # 1. generate permutations of word array (note: don't return itself)
+  # 2. check if word is a real word using the syntax below:
+  # if "word" in words.words():
+  #  print word
+  
+  
+anagrams_for("cinema")
