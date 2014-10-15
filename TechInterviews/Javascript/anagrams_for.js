2d2e54904554092bd477de3edbfdb8145243b243
Christie Ewen <christieewen@users.noreply.github.com>
2014-10-14 21:08:26 -0400

    Create anagrams_for.js
    Initial commit.

---
+++ TechInterviews/Javascript/anagrams_for.js
@@ -0,0 +1,27 @@
+// Anagrams
+//
+// An anagram is a word formed by rearranging the letters of another word, e.g., iceman is an anagram of cinema. 
+// We're going to write a method called anagrams_for that takes as its input a word and an array of words, 
+// representing a dictionary, and returns an array consisting of all the anagrams of the input word. anagrams_for should 
+// return an empty array ([]) if no anagrams are found in the dictionary. You don't have to worry about the order of the 
+// returned Array.
+// http://meetupresources.herokuapp.com/whiteboard.html
+//
+// array methods implementation
+// TODO: hash method implementation
+//
+var dictionary = ["cinema", "iceman"];
+
+function anagrams_for(word) {
+  var new_word = word;
+  // TODO: iterate through each character shifting/unshifting characters, check if word exists to print
+  for (i=0; i < new_word.length; i++) {
+      if (new_word in dictionary) 
+        print word;
+      }
+
+}
+
+
+
+anagrams_for("cinema");
