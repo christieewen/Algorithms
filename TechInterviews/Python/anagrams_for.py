b0721bd1b987087db3347870a73a0fe6c8431ec5
Christie Ewen <christieewen@users.noreply.github.com>
2014-10-16 13:30:17 -0400

    Update anagrams_for.py
    Using builtin and list comprehension.  Haven't tested this code.
    TODO: Implement algorithm for the permutations.

--- TechInterviews/Python/anagrams_for.py
+++ TechInterviews/Python/anagrams_for.py
@@ -1,3 +1,4 @@
+#Python 3.4.2
 from nltk.corpus import words
 from itertools import permutations
 
@@ -7,6 +8,12 @@ def anagrams_for(word):
   # 2. check if word is a real word using the syntax below:
   # if "word" in words.words():
   #  print word
-  
-  
+  # example using itertools: list(itertools.permutations([1,2,3,4], 2))
+  word_array = array('u', word)
+  [print new_word.tobytes() 
+    for new_word in 
+      list(itertools.permutations(word_array, word_array.length)) 
+      if new_word in 
+        words.words())]
+    
 anagrams_for("cinema")
