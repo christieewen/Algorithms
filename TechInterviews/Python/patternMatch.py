17e2758b201037404f7ed9da06ff74997886e512
Christie Ewen <christieewen@users.noreply.github.com>
2015-01-30 23:54:33 -0500

    Update patternMatch.py
    Read N and M lines.

--- TechInterviews/Python/patternMatch.py
+++ TechInterviews/Python/patternMatch.py
@@ -1,10 +1,15 @@
 
-
-
 # Example to call this program: python33 patternMatch.py <input_file> output_file 
 def main(args):
     input_file = open(args[1], 'r')
     output_file = open(args[2], 'w')
+    N = input_file.readline()
+    for N in input_file:
+        input_file.readline()
+    M = input_file.readline()
+    for M in input_file:
+        input_file.readline()
+    
 
 if __name__ == '__main__':
     main(sys.argv)
