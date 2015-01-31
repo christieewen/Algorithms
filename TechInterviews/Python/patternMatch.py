531336af0e48855fceb675042bef9659b58f673d
Christie Ewen <christieewen@users.noreply.github.com>
2015-01-30 23:24:53 -0500

    Create patternMatch.py
    Initial draft of input and output files read from args

---
+++ TechInterviews/Python/patternMatch.py
@@ -0,0 +1,11 @@
+
+
+
+# Example to call this program: python33 patternMatch.py <input_file> output_file 
+def main(args):
+    input_file = open(args[1], 'r')
+    output_file = open(args[2], 'w')
+
+if __name__ == '__main__':
+    main(sys.argv)
+    
