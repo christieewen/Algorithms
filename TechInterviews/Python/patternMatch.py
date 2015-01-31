ea8d084a96e9f8faad8d2d0f18957cadd49fed74
Christie Ewen <christieewen@users.noreply.github.com>
2015-01-31 08:43:58 -0500

    Update patternMatch.py
    Using list as the data structure

--- TechInterviews/Python/patternMatch.py
+++ TechInterviews/Python/patternMatch.py
@@ -8,12 +8,17 @@ def findBestMatch(pattern, path):
 def main(args):
     input_file = open(args[1], 'r')
     output_file = open(args[2], 'w')
+    
+    pattern_list = []
+    path_list = []
+    
     N = input_file.readline()
     for N in input_file:
-        input_file.readline()
+        pattern_list.append(input_file.readline())
+    
     M = input_file.readline()
     for M in input_file:
-        input_file.readline()
+        path_list.append(input_file.readline())
     
 if __name__ == '__main__':
     main(sys.argv)
